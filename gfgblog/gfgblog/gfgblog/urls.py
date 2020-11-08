cfd5c420e5c916b571127541cb1bae46751c4423
ricci22 <nandarizki.nasution@gmail.com>
2020-11-08 19:24:12 +0700

    20201108 Django admin route

--- gfgblog/gfgblog/gfgblog/urls.py
+++ gfgblog/gfgblog/gfgblog/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+    # urls handling admin route
     path('admin/', admin.site.urls),
+    # urls handling blog route
+    path('', include('blog.urls')),
 ]
