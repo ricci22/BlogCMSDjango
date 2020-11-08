eba6a3df212c1d11e18df8613859bef63a745835
ricci22 <nandarizki.nasution@gmail.com>
2020-11-08 10:10:57 +0700

    20201108 run Django

---
+++ gfgblog/gfgblog/gfgblog/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for gfgblog project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gfgblog.settings')
+
+application = get_asgi_application()
